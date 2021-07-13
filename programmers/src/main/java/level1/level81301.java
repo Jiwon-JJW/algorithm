3545880a20df3ae5c658831fc3477e21b6d08ab0
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-07-13 18:45:55 +0900

    level1 Test

---
+++ programmers/src/main/java/level1/level81301.java
@@ -0,0 +1,43 @@
+package level1;
+
+public class level81301 {
+    class Solution2 {
+        public int solution(String s) {
+            if (s.contains("one")) {
+                s = s.replaceAll("one", "1");
+            }
+
+            if (s.contains("two")) {
+                s = s.replaceAll("two", "2");
+            }
+            if (s.contains("three")) {
+                s = s.replaceAll("three", "3");
+            }
+            if (s.contains("four")) {
+                s = s.replaceAll("four", "4");
+            }
+            if (s.contains("five")) {
+                s = s.replaceAll("five", "5");
+            }
+            if (s.contains("six")) {
+                s = s.replaceAll("six", "6");
+            }
+            if (s.contains("seven")) {
+                s = s.replaceAll("seven", "7");
+            }
+            if (s.contains("eight")) {
+                s = s.replaceAll("eight", "8");
+            }
+            if (s.contains("nine")) {
+                s = s.replaceAll("nine", "9");
+            }
+
+            if (s.contains("zero")) {
+                s = s.replaceAll("zero", "0");
+            }
+
+            int answer = Integer.parseInt(s);
+            return answer;
+        }
+    }
+}
