8534d0bc7b9346af36ba58b1608cf99e6b9cbc7e
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2021-10-28 18:44:42 +0900

    leetCode DFS/BFS

--- leetcode/src/main/java/FloodFill.java
+++ leetcode/src/main/java/FloodFill.java
@@ -1,33 +1,80 @@
 public class FloodFill {
-    private int[][] answer;
-    private int mainColor;
-    private int newColor;
+//    private int[][] answer;
+//    private int mainColor;
+//    private int newColor;
+//
+//    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+//        this.answer = image;
+//        this.mainColor = image[sr][sc];
+//        this.newColor = newColor;
+//
+//        if (mainColor == newColor) {
+//            return image;
+//        }
+//
+//        dfs(sr, sc);
+//        return answer;
+//    }
+//
+//    public void dfs(int sr, int sc) {
+//        if (sr < 0 || sc < 0 || sr >= answer.length || sc >= answer[sr].length ||
+//                answer[sr][sc] != mainColor) {
+//            return;
+//        }
+//
+//        answer[sr][sc] = newColor;
+//
+//        // 인접하는 곳의 mainColor를 image에서 탐색하며 컬러를 바꿈.
+//        dfs(sr + 1, sc);
+//        dfs(sr - 1, sc);
+//        dfs(sr, sc + 1);
+//        dfs(sr, sc - 1);
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    int[][] imageC;
+    int mainColor;
+    int newColorC;
 
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        this.answer = image;
-        this.mainColor = image[sr][sc];
-        this.newColor = newColor;
-
-        if (mainColor == newColor) {
-            return image;
-        }
+        imageC = image;
+        mainColor = image[sr][sc];
+        newColorC = newColor;
 
         dfs(sr, sc);
-        return answer;
+
+        return imageC;
     }
 
     public void dfs(int sr, int sc) {
-        if (sr < 0 || sc < 0 || sr >= answer.length || sc >= answer[sr].length ||
-                answer[sr][sc] != mainColor) {
+        if(sr < 0 || sc < 0 || sr >= imageC.length || sc >= imageC[0].length
+                || imageC[sr][sc] == newColorC || imageC[sr][sc] != mainColor) {
             return;
         }
 
-        answer[sr][sc] = newColor;
+        imageC[sr][sc] = newColorC;
 
-        // 인접하는 곳의 mainColor를 image에서 탐색하며 컬러를 바꿈.
-        dfs(sr + 1, sc);
         dfs(sr - 1, sc);
-        dfs(sr, sc + 1);
+        dfs(sr + 1, sc);
         dfs(sr, sc - 1);
+        dfs(sr, sc + 1);
     }
+
 }
--- leetcode/src/main/java/MaxAreaOfIsland.java
+++ leetcode/src/main/java/MaxAreaOfIsland.java
@@ -1,17 +1,55 @@
 public class MaxAreaOfIsland {
-    int x, y;
+//    int x, y;
+//
+//    public int maxAreaOfIsland(int[][] grid) {
+//        x = grid.length;
+//        y = grid[0].length;
+//
+//        int max = 0;
+//
+//        for (int i = 0; i < x; i++) {
+//            for (int j = 0; j < y; j++) {
+//                if (grid[i][j] == 1) {
+//                    int area = dfs(grid, i, j, 0);
+//                    max = Math.max(area, max);
+//                }
+//            }
+//        }
+//
+//        return max;
+//    }
+//
+//    public int dfs(int[][] grid, int i, int j, int area) {
+//        if (i < 0 || j < 0 || i >= x || j >= y ||
+//                grid[i][j] == 0) return area;
+//
+//        area++;
+//
+//        grid[i][j] = 0;
+//        area = dfs(grid, i - 1, j, area);
+//        area = dfs(grid, i + 1, j, area);
+//        area = dfs(grid, i, j - 1, area);
+//        area = dfs(grid, i, j + 1, area);
+//
+//        return area;
+//    }
+
+
+    int[][] grid;
+    int x; int y;
 
     public int maxAreaOfIsland(int[][] grid) {
+        this.grid = grid;
         x = grid.length;
         y = grid[0].length;
 
-        int max = 0;
+        int max =0;
 
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                if (grid[i][j] == 1) {
-                    int area = dfs(grid, i, j, 0);
-                    max = Math.max(area, max);
+        for(int i = 0; i < x; i++) {
+            for(int j = 0; j < y; j++) {
+                if(grid[i][j] == 1) {
+                    int area = dfs(i,j,0);
+                    max = Math.max(area , max);
                 }
             }
         }
@@ -19,18 +57,20 @@ public class MaxAreaOfIsland {
         return max;
     }
 
-    public int dfs(int[][] grid, int i, int j, int area) {
-        if (i < 0 || j < 0 || i >= x || j >= y ||
-                grid[i][j] == 0) return area;
-
+    private int dfs(int i, int j, int area) {
+        if(i < 0 || j < 0 || i >= x || j >= y || grid[i][j] == 0) {
+            return area;
+        }
         area++;
-
         grid[i][j] = 0;
-        area = dfs(grid, i - 1, j, area);
-        area = dfs(grid, i + 1, j, area);
-        area = dfs(grid, i, j - 1, area);
-        area = dfs(grid, i, j + 1, area);
+
+        area = dfs(i + 1, j, area);
+        area = dfs(i - 1, j, area);
+        area = dfs(i, j + 1, area);
+        area = dfs(i, j - 1, area);
 
         return area;
     }
+
+
 }
